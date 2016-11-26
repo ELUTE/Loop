bcd51464df3054917805a211c4571426315a46a2
ELUTE <katefarnsworth@Kates-MacBook-Pro-3.local>
2016-11-25 21:17:04 -0500

    add pebble support

--- Loop/AppDelegate.swift
+++ Loop/AppDelegate.swift
@@ -10,61 +10,93 @@ import UIKit
 import UserNotifications
 import CarbKit
 import InsulinKit
+import PebbleKit
+
 
 @UIApplicationMain
-final class AppDelegate: UIResponder, UIApplicationDelegate {
-
+final class AppDelegate: UIResponder, UIApplicationDelegate, PBPebbleCentralDelegate {
+    
     var window: UIWindow?
-
+    var pebbleCentral : PBPebbleCentral?
+    var watch: PBWatch?
+    
+    
     private(set) lazy var dataManager = DeviceDataManager()
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window?.tintColor = UIColor.tintColor
-
+        
         NotificationManager.authorize(delegate: self)
-
+        
         AnalyticsManager.sharedManager.application(application, didFinishLaunchingWithOptions: launchOptions)
-
+        
         if  let navVC = window?.rootViewController as? UINavigationController,
             let statusVC = navVC.viewControllers.first as? StatusTableViewController {
             statusVC.dataManager = dataManager
         }
-
+        
+        pebbleCentral = PBPebbleCentral.default()
+        pebbleCentral?.delegate = self
+        var uuidBytes = Array<UInt8>(repeating: 0, count: 16)
+        let uuid = UUID(uuidString: "620f2467-c17e-4350-b337-df856e89a134")
+        (uuid as NSUUID?)?.getBytes(&uuidBytes)
+        pebbleCentral?.appUUID = uuid
+        pebbleCentral?.run()
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-
+        
         dataManager.transmitter?.resumeScanning()
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
     func applicationShouldRequestHealthAuthorization(_ application: UIApplication) {
-
+        
     }
-
+    
     // MARK: - 3D Touch
-
+    
     func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
         completionHandler(false)
+    }
+    
+    //MARK: - PEBBLE
+    func pebbleCentral(_ central: PBPebbleCentral, watchDidConnect watch: PBWatch, isNew: Bool) {
+        print("Pebble did connect: \(watch.name)")
+        self.watch = watch
+        self.watch?.appMessagesLaunch({ (watch, error) in
+            
+        })
+        self.watch?.appMessagesAddReceiveUpdateHandler{ (watch,command) -> Bool in
+            return true
+        }
+    }
+    
+    func pebbleCentral(_ central: PBPebbleCentral, watchDidDisconnect watch: PBWatch) {
+        print("Pebble did disconnect: \(watch.name)")
+        if (self.watch == watch) {
+            self.watch = nil
+        }
     }
 }
 
@@ -78,12 +110,12 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
                 startDate.timeIntervalSinceNow >= TimeInterval(minutes: -5)
             {
                 AnalyticsManager.sharedManager.didRetryBolus()
-
+                
                 dataManager.enactBolus(units: units) { (error) in
                     if error != nil {
                         NotificationManager.sendBolusFailureNotificationForAmount(units, atStartDate: startDate)
                     }
-
+                    
                     completionHandler()
                 }
                 return
@@ -94,7 +126,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         
         completionHandler()
     }
-
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.badge, .sound, .alert])
     }
